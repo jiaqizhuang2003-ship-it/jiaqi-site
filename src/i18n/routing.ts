02859bf59f4e1dd748d69dfbab7794d2401a470e
jiaqizhuang2003-ship-it <jiaqizhuang2003@gmail.com>
2026-06-18 02:20:52 +0800

    Default to English locale

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -5,6 +5,7 @@ export const routing = defineRouting({
   locales: ["en", "zh"],
   defaultLocale: "en",
   localePrefix: "always",
+  localeDetection: false,
 });
 
 export type Locale = (typeof routing.locales)[number];
